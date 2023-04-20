b7780b818fcfb3e4e9a9849156ce1b419ae9d08c
shreyas-6471 <37994152+shreyas-6471@users.noreply.github.com>
2023-04-20 09:34:29 -0400

    Adding customer models

---
+++ models/customer.js
@@ -0,0 +1,25 @@
+ const mongoose=require('mongoose');
+const multer=require('multer');
+const path=require('path');
+const AVATAR_PATH=path.join('/uploads/users/avatars');
+const customerSchema=new mongoose.Schema({
+    amount: { type: String, required: true },
+    income: { type: String, required: true },
+    debt: { type: String, required: true },
+    history: { type: String, required: true },
+    accounts: { type: String, required: true },
+    balance: { type: String, required: true },
+    maxcredit: { type: String, required: true },
+    job: { type: String, required: true },
+    home: { type: String, required: true },
+    purpose: { type: String, required: true },
+    problems: { type: String, required: true },
+    bankruptcies: { type: String, required: true },
+    taxliens: { type: String, required: true },
+    creditranges: { type: String, required: true },
+    predictedloanstatus:{type:String,required:true},
+},{
+    timestamps:true
+});
+const Customer=mongoose.model('Customer',customerSchema);
+module.exports=Customer;
